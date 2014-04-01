fe83d96ca4e94f677cbbd6dc1269d623977ff8f7
dtsbourg <dylan.bourgeois@epfl.ch>
2014-04-01 17:20:13 +0200

    Initial Commit

---
+++ Resistor/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Resistor
+//
+//  Created by Dylan Bourgeois on 01/04/14.
+//  Copyright (c) 2014 Dylan Bourgeois. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
